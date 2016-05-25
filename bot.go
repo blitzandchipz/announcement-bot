88d32273ecc9ca934d80196367510a2167e9a363
jared <jared@eddi.com>
2016-05-24 23:02:55 -0700

    Read config settings from file

--- bot.go
+++ bot.go
@@ -5,35 +5,67 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"stathat.com/c/jconfig"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-const (
-	Hostname = "https://api.meetup.com/"
-)
+// Hostname for meetup.com's api
+const Hostname = "https://api.meetup.com/"
 
 var (
-	Email    string
+	// Email for discord user account
+	Email string
+	// Password for discord user account
 	Password string
-	Token    string
-	BotID    string
-	APIKey   string
+	// Token for discord bot account
+	Token string
+	// BotID of the user account
+	BotID string
+	// APIKey for meetup.com
+	APIKey string
+	// GroupURL for a meetup group
 	GroupURL string
+	// Config for the bots settings
+	Config *jconfig.Config
 )
 
-func init() {
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
+func init() {
 	flag.StringVar(&Email, "e", "", "Account Email")
 	flag.StringVar(&Password, "p", "", "Account Password")
 	flag.StringVar(&Token, "t", "", "Account Token")
 	flag.StringVar(&APIKey, "a", "", "Meetup API Key")
 	flag.Parse()
+
+	Config = jconfig.LoadConfig("config.json")
+
+	if Config != nil {
+		if Email == "" && Config.GetString("Email") != "" {
+			Email = Config.GetString("Email")
+		}
+
+		if Password == "" && Config.GetString("Password") != "" {
+			Password = Config.GetString("Password")
+		}
+
+		if Token == "" && Config.GetString("Token") != "" {
+			Token = Config.GetString("Token")
+		}
+
+		if APIKey == "" && Config.GetString("APIKey") != "" {
+			APIKey = Config.GetString("APIKey")
+		}
+	}
 }
 
 func main() {
-
 	// Create a new Discord session using the provided login information.
 	dg, err := discordgo.New(Email, Password, Token)
 	if err != nil {
@@ -71,11 +103,18 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	// If the message is "ping" reply with "Pong!"
+	// Sets the meetup group needed for future commands
 	if strings.HasPrefix(m.Content, "!setgroup") {
 		GroupURL = strings.TrimSpace(strings.TrimPrefix(m.Content, "!setgroup"))
 		s.ChannelMessageSend(m.ChannelID, "Group url now set to: "+GroupURL)
-	} else if strings.HasPrefix(m.Content, "!getevents") {
+	}
+
+	// Gets a list of events for the currently set group
+	if strings.HasPrefix(m.Content, "!getevents") {
+		if GroupURL == "" {
+			s.ChannelMessageSend(m.ChannelID, "Run !setgroup first")
+			return
+		}
 		url := Hostname + GroupURL + "/events?key=" + APIKey + "?page=3"
 		events, err := http.Get(url)
 		if err != nil {
