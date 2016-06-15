1cb8b1ef8c453299eaa7a0723f62383809e3d972
jared <jared@eddi.com>
2016-06-14 17:07:18 -0700

    Finished nextevent command, check for invalid group urlname

--- bot.go
+++ bot.go
@@ -33,10 +33,47 @@ var (
 	Config *jconfig.Config
 )
 
+// Event is a single event from meetup.com
+type Event struct {
+	ID            string `json:"id"`
+	Name          string `json:"name"`
+	Status        string `json:"status"`
+	Time          int64  `json:"time"`
+	Updated       int64  `json:"updated"`
+	UTCOffset     int64  `json:"utc_offset"`
+	WaitlistCount int    `json:"waitlist_count"`
+	YesRSVPCount  int    `json:"yes_rsvp_count"`
+	Venue         Venue  `json:"venue"`
+	Link          string `json:"link"`
+	Description   string `json:"description"`
+	Visibility    string `json:"visibility"`
+}
+
+// Venue is a location where events happen
+type Venue struct {
+	ID                   int     `json:"id"`
+	Name                 string  `json:"name"`
+	Lat                  float64 `json:"lat"`
+	Lon                  float64 `json:"lon"`
+	Repinned             bool    `json:"repinned"`
+	Address1             string  `json:"address_1"`
+	Address2             string  `json:"address_2"`
+	City                 string  `json:"city"`
+	Country              string  `json:"country"`
+	LocalizedCountryName string  `json:"localized_country_name"`
+	Zip                  string  `json:"zip"`
+	State                string  `json:"state"`
+}
+
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func errMsg(err error) string {
+	msg := err.Error()
+	return msg
 }
 
 func init() {
@@ -65,8 +102,6 @@ func init() {
 			APIKey = Config.GetString("APIKey")
 		}
 	}
-
-	fmt.Print(APIKey)
 }
 
 func main() {
@@ -101,20 +136,39 @@ func main() {
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
 	// Ignore all messages created by the bot itself
 	if m.Author.ID == BotID {
 		return
 	}
 
 	// Sets the meetup group needed for future commands
+	// TODO Add permissions: only admins should be able to set the group for
+	// the server
 	if strings.HasPrefix(m.Content, "!setgroup") {
-		// TODO handle error of invalid group
-		GroupURL = strings.TrimSpace(strings.TrimPrefix(m.Content, "!setgroup"))
+		tempURL := strings.TrimSpace(strings.TrimPrefix(m.Content, "!setgroup"))
+		url := Hostname + "/" + tempURL + "?key=" + APIKey
+		resp, err := http.Get(url)
+		if err != nil {
+			errMsg := errMsg(err)
+			s.ChannelMessageSend(m.ChannelID, errMsg)
+			return
+		}
+		defer resp.Body.Close()
+
+		// meetup 404s on nonexistent group names
+		if resp.StatusCode == 404 {
+			// TODO pull error message from meetup's json response
+			s.ChannelMessageSend(m.ChannelID, "Invalid group urlname")
+			return
+		}
+		// Store the group in global
+		// TODO rework this so it works per discord guild
+		GroupURL = tempURL
 		s.ChannelMessageSend(m.ChannelID, "Group url now set to: "+GroupURL)
 	}
 
 	// Gets a list of events for the currently set group
+	// TODO: finish outputt message to server
 	if strings.HasPrefix(m.Content, "!getevents") {
 		if GroupURL == "" {
 			s.ChannelMessageSend(m.ChannelID, "Run !setgroup first")
@@ -133,8 +187,12 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		s.ChannelMessageSend(m.ChannelID, string(contents))
 	}
 
-	// Returns the next upcoming events
+	// Returns the next upcoming, public event
 	if strings.HasPrefix(m.Content, "!nextevent") {
+		if GroupURL == "" {
+			s.ChannelMessageSend(m.ChannelID, "Run !setgroup first")
+			return
+		}
 		url := Hostname + GroupURL + "/events?key=" + APIKey + "&page=1"
 		var events []Event
 		err := getJSON(url, &events)
@@ -145,11 +203,30 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 		msg := "No future, public events found"
 
+		// Check if theres any events
 		if len(events) > 0 {
 			event := events[0]
+			// Only consider public and upcoming events
 			if (event.Visibility == "public") && (event.Status == "upcoming") {
+				venueStr := ""
+				// Check if a venue exists
+				if event.Venue.Name != "" {
+					venue := event.Venue
+					// Just print the name if there's no address
+					// TODO Print address even if there's no name
+					if venue.Address1 == "" {
+						venueStr = fmt.Sprintf("\nAt: `%v`", venue.Name)
+					} else {
+						// Print full location details
+						// TODO test for missing location information
+						venueStr = fmt.Sprintf("\nAt: `%v` - %v %v, %v %v",
+							venue.Name, venue.Address1, venue.City, venue.State, venue.Zip)
+					}
+				}
 				time, _ := msToTime(event.Time)
-				msg = fmt.Sprintf("Next event: `%v`\nOn: `%v`", event.Name, time)
+				// description := truncate(event.Description)
+				msg = fmt.Sprintf("Next event: `%v` - %v%v\n%v",
+					event.Name, time, venueStr, event.Link)
 			}
 		}
 
@@ -157,21 +234,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 }
 
-// Event is a single event
-type Event struct {
-	ID            string `json:"id"`
-	Name          string `json:"name"`
-	Status        string `json:"status"`
-	Time          int64  `json:"time"`
-	Updated       int    `json:"updated"`
-	UTCOffset     int    `json:"utc_offset"`
-	WaitlistCount int    `json:"waitlist_count"`
-	YesRSVPCount  int    `json:"yes_rsvp_count"`
-	Link          string `json:"link"`
-	Description   string `json:"description"`
-	Visibility    string `json:"visibility"`
-}
-
+// Helper function to unmarshle json data into structs
 func getJSON(url string, target interface{}) error {
 	r, err := http.Get(url)
 	if err != nil {
@@ -182,16 +245,15 @@ func getJSON(url string, target interface{}) error {
 	return json.NewDecoder(r.Body).Decode(target)
 }
 
-func errMsg(err error) string {
-	msg := err.Error()
-	return msg
+// Helper function to truncates a string and adds ellipsis
+func truncate(str string) string {
+	if len(str) > 150 {
+		return string(str[:47] + "...")
+	}
+	return str
 }
 
+// Helper function to convert ms since epoch to ANSIC time format
 func msToTime(ms int64) (string, error) {
-	// msInt, err := strconv.ParseInt(ms, 10, 6Teensy4)
-	// if err != nil {
-	// 	return time.Time{}, err
-	// }
-
 	return time.Unix(0, ms*int64(time.Millisecond)).Format(time.ANSIC), nil
 }
